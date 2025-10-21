bf2dcb5ced8d82231803efbfbdad0ba3ae1f110f
Thisisnnn <kyhia.tran@oteria.fr>
2025-10-21 17:46:35 +0200

    Erased the unused lines

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
 	import {
 		Dropdown,
 		DropdownItem,
